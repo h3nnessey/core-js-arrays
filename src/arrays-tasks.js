f5c7f291cc150aa97528ed18c487ac963bb513fc
Vitaly <vitaliy.zayakin@gmail.com>
2024-12-05 23:50:43 +0500

    feat: pass 45 test cases

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -485,8 +485,19 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  return nums.reduce(
+    (acc, curr, index) => {
+      if (curr > nums[index - 1]) {
+        acc.count += 1;
+      } else {
+        acc.count = 1;
+      }
+
+      return acc.count > acc.max ? { ...acc, max: acc.count } : acc;
+    },
+    { count: 1, max: 0 }
+  ).max;
 }
 
 /**
